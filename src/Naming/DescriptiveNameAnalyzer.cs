217fb0fadd570c1ae3edaeb33970c2feaac3c7a0
Vitalii Kotenko <caunt.official@gmail.com>
2026-09-21 03:29:51 +0300

    remove id from abbreviated words

--- src/Naming/DescriptiveNameAnalyzer.cs
+++ src/Naming/DescriptiveNameAnalyzer.cs
@@ -38,7 +38,7 @@ public sealed class DescriptiveNameAnalyzer() : PolicyAnalyzer(Rule)
     private static readonly ImmutableHashSet<string> AbbreviatedWords = ImmutableHashSet.Create(
         StringComparer.OrdinalIgnoreCase,
         items: [        "args", "auth", "cfg", "config", "ctx", "db", "doc", "docs", "dto", "dsp",
-        "env", "html", "id", "ids", "info", "init", "io", "ip", "json", "max", "min",
+        "env", "html", "ids", "info", "init", "io", "ip", "json", "max", "min",
         "msg", "num", "opts", "param", "params", "prev", "proc", "ptr", "req", "res", "sdr", "sql",
         "temp", "tmp", "ui", "utils", "var"]
     );
